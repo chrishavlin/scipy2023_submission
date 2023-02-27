a6a6684090d5853cc61cca4553075743e6d0757e
Chris Havlin <chris.havlin@gmail.com>
2023-02-27 09:24:28 -0600

    environment setup

---
+++ check_env.py
@@ -0,0 +1,8 @@
+import yt
+import yt_xarray
+import cartopy 
+from yt_xarray.utilities.logging import ytxr_log
+import matplotlib.pyplot as plt 
+
+if __name__=="__main__":
+    ytxr_log.info("yt_xarray_scipy2023 installation successful")    
